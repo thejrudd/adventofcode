03a15d676c828ba32449843e06134625f2430eb3
thejrudd <the.jrudd@gmail.com>
2021-12-04 20:54:44 -0800

    Finalized solve for day3 puzzle 1 of Advent of Code
    I hate how verbose this is.

--- day3/puzzle1/day3puzzle1solve.py
+++ day3/puzzle1/day3puzzle1solve.py
@@ -25,7 +25,7 @@ sum10 = 0
 sum11 = 0
 sum12 = 0
 
-with open("puzzleinput") as file:
+with open("day3/puzzle1/puzzleinput") as file:
     for element in file.readlines():
         position1.append(element[0])
         position2.append(element[1])
@@ -68,98 +68,127 @@ else:
     epsilon3 = 0
 
 for item in position4:
-    sum4 = int(item) + sum
-if sum4 < ((len(position4)/2)):                                                                        │jruddick@JRUDDICK-ZBX:~/teaching-sand-to-think/advent/day3/puzzle1$ ./day3puzzle1solve.py
-    gamma4 = 0                                                                                         │1000
-    epsilon4 = 1                                                                                       │487
-else:                                                                                                  │0
-    gamma4 = 1                                                                                         │jruddick@JRUDDICK-ZBX:~/teaching-sand-to-think/advent/day3/puzzle1$
+    sum4 = int(item) + sum4
+if sum4 < ((len(position4)/2)):
+    gamma4 = 0
+    epsilon4 = 1
+else:         
+    gamma4 = 1
     epsilon4 = 0
 
-for item in position5:                                                                                 │0
-    sum5 = int(item) + sum5                                                                            │jruddick@JRUDDICK-ZBX:~/teaching-sand-to-think/advent/day3/puzzle1$
-if sum5 < ((len(position5)/2)):                                                                        │jruddick@JRUDDICK-ZBX:~/teaching-sand-to-think/advent/day3/puzzle1$ ./day3puzzle1solve.py
-    gamma5 = 0                                                                                         │1000
-    epsilon5 = 1                                                                                       │487
-else:                                                                                                  │0
-    gamma5 = 1                                                                                         │jruddick@JRUDDICK-ZBX:~/teaching-sand-to-think/advent/day3/puzzle1$
+for item in position5:
+    sum5 = int(item) + sum5
+if sum5 < ((len(position5)/2)):
+    gamma5 = 0             
+    epsilon5 = 1           
+else:                      
+    gamma5 = 1             
     epsilon5 = 0
 
-for item in position6:                                                                                 │0
-    sum6 = int(item) + sum6                                                                            │jruddick@JRUDDICK-ZBX:~/teaching-sand-to-think/advent/day3/puzzle1$
-if sum6 < ((len(position6)/2)):                                                                        │jruddick@JRUDDICK-ZBX:~/teaching-sand-to-think/advent/day3/puzzle1$ ./day3puzzle1solve.py
-    gamma6 = 0                                                                                         │1000
-    epsilon6 = 1                                                                                       │487
-else:                                                                                                  │0
-    gamma6 = 1                                                                                         │jruddick@JRUDDICK-ZBX:~/teaching-sand-to-think/advent/day3/puzzle1$
+for item in position6:     
+    sum6 = int(item) + sum6
+if sum6 < ((len(position6)/2)):
+    gamma6 = 0                 
+    epsilon6 = 1                
+else:                           
+    gamma6 = 1                  
     epsilon6 = 0
 
-for item in position7:                                                                                 │0
-    sum7 = int(item) + sum7                                                                            │jruddick@JRUDDICK-ZBX:~/teaching-sand-to-think/advent/day3/puzzle1$
-if sum7 < ((len(position7)/2)):                                                                        │jruddick@JRUDDICK-ZBX:~/teaching-sand-to-think/advent/day3/puzzle1$ ./day3puzzle1solve.py
-    gamma7 = 0                                                                                         │1000
-    epsilon7 = 1                                                                                       │487
-else:                                                                                                  │0
-    gamma7 = 1                                                                                         │jruddick@JRUDDICK-ZBX:~/teaching-sand-to-think/advent/day3/puzzle1$
+for item in position7:          
+    sum7 = int(item) + sum7     
+if sum7 < ((len(position7)/2)): 
+    gamma7 = 0                  
+    epsilon7 = 1                
+else:                           
+    gamma7 = 1                  
     epsilon7 = 0
 
-for item in position8:                                                                                 │0
-    sum8 = int(item) + sum8                                                                            │jruddick@JRUDDICK-ZBX:~/teaching-sand-to-think/advent/day3/puzzle1$
-if sum8 < ((len(position8)/2)):                                                                        │jruddick@JRUDDICK-ZBX:~/teaching-sand-to-think/advent/day3/puzzle1$ ./day3puzzle1solve.py
-    gamma8 = 0                                                                                         │1000
-    epsilon8 = 1                                                                                       │487
-else:                                                                                                  │0
-    gamma8 = 1                                                                                         │jruddick@JRUDDICK-ZBX:~/teaching-sand-to-think/advent/day3/puzzle1$
+for item in position8:
+    sum8 = int(item) + sum8              
+    
+if sum8 < ((len(position8)/2)):          
+    gamma8 = 0                           
+    epsilon8 = 1                         
+else:                                    
+    gamma8 = 1                           
     epsilon8 = 0
 
-for item in position9:                                                                                 │0
-    sum9 = int(item) + sum9                                                                            │jruddick@JRUDDICK-ZBX:~/teaching-sand-to-think/advent/day3/puzzle1$
-if sum9 < ((len(position9)/2)):                                                                        │jruddick@JRUDDICK-ZBX:~/teaching-sand-to-think/advent/day3/puzzle1$ ./day3puzzle1solve.py
-    gamma9 = 0                                                                                         │1000
-    epsilon9 = 1                                                                                       │487
-else:                                                                                                  │0
-    gamma9 = 1                                                                                         │jruddick@JRUDDICK-ZBX:~/teaching-sand-to-think/advent/day3/puzzle1$
+for item in position9:                   
+    sum9 = int(item) + sum9              
+if sum9 < ((len(position9)/2)):          
+    gamma9 = 0                           
+    epsilon9 = 1                         
+else:                                    
+    gamma9 = 1                           
     epsilon9 = 0
 
-for item in position10:                                                                                 │0
+for item in position10:                  
     sum10 = int(item) + sum10
-if sum10 < ((len(position10)/2)):                                                                        │jruddick@JRUDDICK-ZBX:~/teaching-sand-to-think/advent/day3/puzzle1$ ./day3puzzle1solve.py
-    gamma10 = 0                                                                                         │1000
-    epsilon10 = 1                                                                                       │487
-else:                                                                                                  │0
-    gamma10 = 1                                                                                         │jruddick@JRUDDICK-ZBX:~/teaching-sand-to-think/advent/day3/puzzle1$
+if sum10 < ((len(position10)/2)):        
+    gamma10 = 0                          
+    epsilon10 = 1                        
+else:                                    
+    gamma10 = 1                          
     epsilon10 = 0
 
-for item in position11:                                                                                 │0
-    sum11 = int(item) + sum11                                                                            │jruddick@JRUDDICK-ZBX:~/teaching-sand-to-think/advent/day3/puzzle1$
-if sum11 < ((len(position11)/2)):                                                                        │jruddick@JRUDDICK-ZBX:~/teaching-sand-to-think/advent/day3/puzzle1$ ./day3puzzle1solve.py
-    gamma11 = 0                                                                                         │1000
-    epsilon11 = 1                                                                                       │487
-else:                                                                                                  │0
-    gamma11 = 1                                                                                         │jruddick@JRUDDICK-ZBX:~/teaching-sand-to-think/advent/day3/puzzle1$
+for item in position11:                  
+    sum11 = int(item) + sum11            
+if sum11 < ((len(position11)/2)):        
+    gamma11 = 0                          
+    epsilon11 = 1                        
+else:                                    
+    gamma11 = 1                          
     epsilon11 = 0
 
-for item in position12:                                                                                 │0
-    sum12 = int(item) + sum12                                                                           │jruddick@JRUDDICK-ZBX:~/teaching-sand-to-think/advent/day3/puzzle1$
-if sum12 < ((len(position12)/2)):                                                                        │jruddick@JRUDDICK-ZBX:~/teaching-sand-to-think/advent/day3/puzzle1$ ./day3puzzle1solve.py
-    gamma12 = 0                                                                                         │1000
-    epsilon12 = 1                                                                                       │487
-else:                                                                                                  │0
-    gamma12 = 1                                                                                         │jruddick@JRUDDICK-ZBX:~/teaching-sand-to-think/advent/day3/puzzle1$
+for item in position12:                  
+    sum12 = int(item) + sum12            
+if sum12 < ((len(position12)/2)):        
+    gamma12 = 0                          
+    epsilon12 = 1                        
+else:                                    
+    gamma12 = 1                          
     epsilon12 = 0
 
 gammalist = [
-gamma1, 
-gamma2, 
-gamma3, 
-gamma4, 
-gamma5, 
-gamma6, 
-gamma7, 
-gamma8, 
-gamma9, 
-gamma10, 
-gamma11, 
-gamma12
+    gamma1, 
+    gamma2, 
+    gamma3, 
+    gamma4, 
+    gamma5, 
+    gamma6, 
+    gamma7, 
+    gamma8, 
+    gamma9, 
+    gamma10, 
+    gamma11, 
+    gamma12
 ]
-print(gammalist)
+epsilonlist = [
+    epsilon1, 
+    epsilon2, 
+    epsilon3, 
+    epsilon4, 
+    epsilon5, 
+    epsilon6, 
+    epsilon7, 
+    epsilon8, 
+    epsilon9, 
+    epsilon10, 
+    epsilon11, 
+    epsilon12
+]
+
+gammastringlist = [str(i) for i in gammalist]
+gammavalue = int("".join(gammastringlist))
+gammastringnew = str(gammavalue)
+gammaint = int(gammastringnew, 2)
+
+epsilonstringlist = [str(i) for i in epsilonlist]
+epsilonvalue = int("".join(epsilonstringlist))
+epsilonstringnew = str(epsilonvalue)
+epsilonint = int(epsilonstringnew, 2)
+
+print(gammaint)
+print(epsilonint)
+
+print(gammaint * epsilonint)
