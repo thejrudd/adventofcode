c4c96035d94481997e54163e678fa500a03c7b52
thejrudd <the.jrudd@gmail.com>
2021-12-07 13:18:27 -0800

    Created dictionaries of all column integers as values and all row numbers as keys.

--- day4/puzzle1/day4puzzle1solve.py
+++ day4/puzzle1/day4puzzle1solve.py
@@ -18,19 +18,46 @@ with open("puzzleinput") as file:
     column3 = []
     column4 = []
     column5 = []
+    row_dict1 = {}
+    row_dict2 = {}
+    row_dict3 = {}
+    row_dict4 = {}
+    row_dict5 = {}
     linenumber = 1
     #for element in file_length:
         
     for line in islice(file, 2, None):
-    #for line in file:
+# Creates lists of the input of each column, as two character strings.
         column1.append(line[:2])
         column2.append(line[3:6])
         column3.append(line[6:9])
         column4.append(line[9:12])
         column5.append(line[12:15])
+# Replaces new line list entries with blank space.
     column1 = [s.replace('\n', '') for s in column1]
-    column1 = column1.remove('')
-    print(column1)
+# Deletes blank space '' entries in list    
+    del column1[5::6]
+    del column2[5::6]
+    del column3[5::6]
+    del column4[5::6]
+    del column5[5::6]
+# Reads in column string entries and converts them to integer values
+    for entry in range(0, len(column1)):
+        column1[entry] = int(column1[entry])
+        column2[entry] = int(column2[entry])
+        column3[entry] = int(column3[entry])
+        column4[entry] = int(column4[entry])
+        column5[entry] = int(column5[entry])
+# Creates dictionary where key = row number and value = column entry
+    for row_number in range(0, len(column1)):
+        row_dict1[row_number] = column1[row_number]
+        row_dict2[row_number] = column1[row_number]
+        row_dict3[row_number] = column1[row_number]
+        row_dict4[row_number] = column1[row_number]
+        row_dict5[row_number] = column1[row_number]
+print(len(column4))
+# Resolves which puzzle number we are working on by dividing total rows by 5.
+    #print(column1)
     #print(column2)
     #print(column3)
     #print(puzzleline)
