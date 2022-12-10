4a67ecd3c8912428069c0d2e6fc99fdaaa858405
thejrudd <the.jrudd@gmail.com>
2022-12-09 18:12:56 -0800

    Pushed old version of day4puzzle1solve - incomplete

--- day4/puzzle1/day4puzzle1solvenew.py
+++ day4/puzzle1/day4puzzle1solvenew.py
@@ -7,14 +7,14 @@ puzzlerow = []
 puzzlevalue = []
 
 #Derives number of total puzzles available
-with open("puzzleinput") as file:
+with open("day4/puzzle1/puzzleinput") as file:
     bingoselection = file.readlines()[0].rstrip()
 
-with open("puzzleinput") as file:
+with open("day4/puzzle1/puzzleinput") as file:
     for line in islice(file, 2, None):
         puzzlerow = line.split()
 
-with open ("puzzleinput") as file:
+with open ("day4/puzzle1/puzzleinput") as file:
     for line in file:
 #        number_of_puzzles = (len(file.readlines() - 2)/5)
          puzzlelength = len(file.readlines())
@@ -23,12 +23,9 @@ print(puzzlelength)
 numberofpuzzles = int((puzzlelength) / 6)
 print(numberofpuzzles)
 
-with open ("puzzleinput") as file:
-    for puzzle in range(numberofpuzzles):
-        print(puzzle)
-        for i in file:
+#with open ("day4/puzzle1/puzzleinput") as file:
+#    for puzzle in range(numberofpuzzles):
+#        print(puzzle)
+#        for i in file:
             #print(i)
-            for row in range(5):
-                #print(row)
-
-#print(puzzlerow)
+#            for row in range(5):
